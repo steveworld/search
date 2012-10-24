2798a37583cefdeb0aab4410d690ffbf483a63f4
Wolfgang Hoschek <whoschek@cloudera.com>
2012-10-24 12:07:05 -0700

    initial commit

---
+++ src/main/java/org/apache/flume/sink/solr/SimpleSolrSink.java
@@ -0,0 +1,231 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.flume.sink.solr;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.flume.Channel;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.EventDeliveryException;
+import org.apache.flume.FlumeException;
+import org.apache.flume.Transaction;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.sink.AbstractSink;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import org.apache.solr.client.solrj.impl.ConcurrentUpdateSolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.UpdateResponse;
+import org.apache.solr.common.SolrInputDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Flume sink that extracts search documents from Apache Flume event headers, optionally transforms them and loads
+ * them into Apache Solr.
+ */
+public class SimpleSolrSink extends AbstractSink implements Configurable {
+  
+  private SolrServer server; // proxy to remote solr
+  private CountDownLatch isStopping = new CountDownLatch(1); // indicates we should shutdown ASAP
+  private CountDownLatch isStopped = new CountDownLatch(1); // indicates we are stopped
+  private int numLoadedDocs = 0; // number of documents loaded in the current transaction
+      
+  private final AtomicLong takeTimer = new AtomicLong(); // TODO: use codahale-metrics or org.apache.flume.CounterGroup?
+  private final AtomicLong extractTimer = new AtomicLong();
+  private final AtomicLong transformTimer = new AtomicLong();
+  private final AtomicLong loadTimer = new AtomicLong();
+  
+  private static final Logger LOGGER = LoggerFactory.getLogger(SimpleSolrSink.class);
+
+  public SimpleSolrSink() {
+    this(null);
+  }
+  
+  /** For testing only */
+  protected SimpleSolrSink(SolrServer server) {
+    this.server = server;
+  }
+  
+  @Override
+  public void configure(Context context) {
+  }
+
+  @Override
+  public synchronized void start() {
+    isStopping = new CountDownLatch(1);
+    isStopped = new CountDownLatch(1);
+    if (server == null) {
+      server = createSolrServer();
+    }
+    super.start();
+  }
+
+  protected SolrServer getSolrServer() {
+    return server;
+  }
+  
+  protected SolrServer createSolrServer() {
+    String solrServerUrl = "http://127.0.0.1:8983/solr/collection1";
+    return new HttpSolrServer(solrServerUrl);
+  }
+  
+  @Override
+  public synchronized void stop() {
+    stop(60, TimeUnit.SECONDS);
+  }
+  
+  public synchronized void stop(long timeout, TimeUnit timeunit) {
+    isStopping.countDown(); // start() and stop() are called from an arbitrary async Flume management thread
+    try {
+      if (!isStopped.await(timeout, timeunit)) { // give Flume management thread some time to exit process() gracefully
+        LOGGER.warn("Failed to stop gracefully. Now shutting down anyway.");
+      }
+    } catch (InterruptedException e) {
+      throw new FlumeException(e);
+    }
+    
+    try {
+      SolrServer s = getSolrServer();
+      if (s != null && (!(s instanceof EmbeddedSolrServer))) {
+        s.shutdown();
+      }
+    } finally {
+      server = null;
+      super.stop();
+    }
+  }
+  
+  @Override
+  public Status process() throws EventDeliveryException {    
+    Channel ch = getChannel();
+    Transaction tx = ch.getTransaction();
+    try {
+      tx.begin();
+      int batchSize = getBatchSize();
+      for (int i = 0; i < batchSize; i++) { // repeatedly take and process events from the Flume queue
+        synchronized (this) {
+          if (isStopping.await(0, TimeUnit.NANOSECONDS) || isStopped.await(0, TimeUnit.NANOSECONDS)) {
+            break;
+          }
+        }
+        long startTime = System.nanoTime();
+        Event event = ch.take();
+        takeTimer.addAndGet(System.nanoTime() - startTime);        
+        if (event == null) {
+          break;
+        }
+        LOGGER.debug("solr event: {}", event);
+        process(event);
+      }
+      
+      if (numLoadedDocs > 0) {
+        numLoadedDocs = 0;
+        commit();
+      }
+      tx.commit();
+      synchronized (this) {
+        if (isStopping.await(0, TimeUnit.NANOSECONDS)) {
+          isStopped.countDown();
+        }
+      }
+      return Status.READY;
+    } catch (Exception e) {
+      tx.rollback();
+      throw new EventDeliveryException(e);
+    } finally {
+      tx.close();
+    }    
+  }
+
+  protected int getBatchSize() {
+    return 1000;
+  }
+  
+  /** Extracts, transforms and loads the given Flume event into Solr */
+  protected void process(Event event) throws IOException, SolrServerException {
+    long startTime = System.nanoTime();
+    List<SolrInputDocument> docs = extract(event); // TODO: use queue to support parallel ETL across multiple CPUs?
+    extractTimer.addAndGet(System.nanoTime() - startTime);
+    
+    startTime = System.nanoTime();
+    docs = transform(docs);
+    transformTimer.addAndGet(System.nanoTime() - startTime);
+
+    startTime = System.nanoTime();
+    load(docs);
+    loadTimer.addAndGet(System.nanoTime() - startTime);
+  }
+
+  /** Extracts and transforms the given Flume event into zero or more Solr documents */
+  protected List<SolrInputDocument> extract(Event event) {
+    SolrInputDocument doc = new SolrInputDocument();
+    for (Entry<String, String> entry : event.getHeaders().entrySet()) {
+      doc.setField(entry.getKey(), entry.getValue());
+    }
+    if (event.getBody() != null) {
+      doc.setField("binaryContent", event.getBody());
+    }
+    return Collections.singletonList(doc);
+  }
+
+  /** Extension point to transform a list of documents in an application specific way. Does nothing by default */
+  protected List<SolrInputDocument> transform(List<SolrInputDocument> docs) {
+    return docs;
+  }
+  
+  /** Loads the given documents into Solr */
+  public void load(List<SolrInputDocument> docs) throws IOException, SolrServerException {
+    if (docs.size() > 0) {
+      UpdateResponse rsp = getSolrServer().add(docs);
+      numLoadedDocs += docs.size();
+    }
+  }
+
+  /**
+   * Sends any outstanding documents to solr and waits for a positive or negative ack (i.e. exception) from solr.
+   * Depending on the outcome the caller should then commit or rollback the current flume transaction correspondingly.
+   */
+  protected void commit() {
+    SolrServer s = getSolrServer();
+    if (s instanceof ConcurrentUpdateSolrServer) {
+      try {
+        ((ConcurrentUpdateSolrServer) s).blockUntilFinished();
+      } finally {
+        if (s instanceof SafeConcurrentUpdateSolrServer) {
+          ((SafeConcurrentUpdateSolrServer) s).clearException();
+        }
+      }
+    }
+  }
+  
+  @Override
+  public String toString() { // debug only
+    return "takeTimer[ms]=" + (takeTimer.get() / 1000000) + ", extractTimer[ms]=" + (extractTimer.get() / 1000000) + ", transformTimer[ms]=" + (transformTimer.get() / 1000000) + ", loadTimer[ms]=" + (loadTimer.get() / 1000000);
+  }
+  
+}
---
+++ src/main/java/org/apache/flume/sink/solr/TikaSolrSink.java
@@ -0,0 +1,347 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.flume.sink.solr;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.FlumeException;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.conf.ConfigurationException;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.CloudSolrServer;
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.params.MapSolrParams;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.ContentStreamBase;
+import org.apache.solr.common.util.DateUtil;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.core.PluginInfo;
+import org.apache.solr.core.SolrConfig;
+import org.apache.solr.handler.extraction.ExtractingParams;
+import org.apache.solr.handler.extraction.ExtractingRequestHandler;
+import org.apache.solr.handler.extraction.RegexRulesPasswordProvider;
+import org.apache.solr.handler.extraction.SolrContentHandler;
+import org.apache.solr.request.SolrRequestHandler;
+import org.apache.solr.schema.IndexSchema;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.DefaultParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.PasswordProvider;
+import org.apache.tika.sax.TeeContentHandler;
+import org.apache.tika.sax.XHTMLContentHandler;
+import org.apache.tika.sax.xpath.Matcher;
+import org.apache.tika.sax.xpath.MatchingContentHandler;
+import org.apache.tika.sax.xpath.XPathParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+
+/**
+ * Flume sink that extracts search documents from Apache Flume events (using Apache Tika and Solr Cell), optionally
+ * transforms them and loads them into Apache Solr.
+ */
+public class TikaSolrSink extends SimpleSolrSink implements Configurable {
+    
+  protected String zkConnectString = null;
+  protected String solrServerUrl = "http://127.0.0.1:8983/solr/collection1";
+  protected int solrServerNumThreads = 2;
+  protected int solrServerQueueLength = solrServerNumThreads;
+  protected int solrServerBatchSize = 1000;
+  
+  private TikaConfig config;
+  private IndexSchema schema;
+  private SolrParams params = new MapSolrParams(new HashMap());
+  private AutoDetectParser autoDetectParser;  
+  private Collection<String> dateFormats = DateUtil.DEFAULT_DATE_FORMATS;
+    
+  private static final XPathParser PARSER = new XPathParser("xhtml", XHTMLContentHandler.XHTML);
+
+  public static final String SOLR_CLIENT_HOME = "solr.home";  
+  public static final String SOLR_ZK_CONNECT_STRING = "zkConnectString";
+  public static final String SOLR_SERVER_URL = "url";
+  public static final String SOLR_SERVER_QUEUE_LENGTH = "queueLength";
+  public static final String SOLR_SERVER_NUM_THREADS = "numThreads";
+  public static final String SOLR_SERVER_BATCH_SIZE = "batchSize";
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TikaSolrSink.class);
+
+  public TikaSolrSink() {
+    this(null);
+  }
+  
+  /** For testing only */
+  protected TikaSolrSink(SolrServer server) {
+    super(server);
+  }
+  
+  @Override
+  public void configure(Context context) {
+    try {
+      String solrHome = context.getString(SOLR_CLIENT_HOME);
+      if (solrHome != null) {
+        System.setProperty("solr.solr.home", solrHome);
+      }
+      
+      SolrConfig solrConfig = new SolrConfig();
+//      SolrConfig solrConfig = new SolrConfig("solrconfig.xml");
+      //SolrConfig solrConfig = new SolrConfig("/cloud/apache-solr-4.0.0-BETA/example/solr/collection1", "solrconfig.xml", null);
+      //SolrConfig solrConfig = new SolrConfig("/cloud/apache-solr-4.0.0-BETA/example/solr/collection1/conf/solrconfig.xml");
+      
+      schema = new IndexSchema(solrConfig, null, null);
+      //schema = new IndexSchema(solrConfig, "schema.xml", null);
+      //schema = new IndexSchema(solrConfig, "/cloud/apache-solr-4.0.0-BETA/example/solr/collection1/conf/schema.xml", null);
+
+      for (PluginInfo pluginInfo : solrConfig.getPluginInfos(SolrRequestHandler.class.getName())) {
+        if ("/update/extract".equals(pluginInfo.name)) {
+          NamedList initArgs = pluginInfo.initArgs;
+          
+          // Copied from StandardRequestHandler 
+          if (initArgs != null) {
+            Object o = initArgs.get("defaults");
+            if (o != null && o instanceof NamedList) {
+              SolrParams defaults = SolrParams.toSolrParams((NamedList)o);              
+              params = defaults;
+            }
+            o = initArgs.get("appends");
+            if (o != null && o instanceof NamedList) {
+              SolrParams appends = SolrParams.toSolrParams((NamedList)o);
+            }
+            o = initArgs.get("invariants");
+            if (o != null && o instanceof NamedList) {
+              SolrParams invariants = SolrParams.toSolrParams((NamedList)o);
+            }
+            o = initArgs.get("server");
+            if (o != null && o instanceof NamedList) {
+              SolrParams solrServerParams = SolrParams.toSolrParams((NamedList)o);
+              zkConnectString = solrServerParams.get(SOLR_ZK_CONNECT_STRING, zkConnectString);
+              solrServerUrl = solrServerParams.get(SOLR_SERVER_URL, solrServerUrl);
+              solrServerNumThreads = solrServerParams.getInt(SOLR_SERVER_NUM_THREADS, solrServerNumThreads);
+              solrServerQueueLength = solrServerParams.getInt(SOLR_SERVER_QUEUE_LENGTH, solrServerNumThreads);
+              solrServerBatchSize = solrServerParams.getInt(SOLR_SERVER_BATCH_SIZE, solrServerBatchSize);
+            }
+            
+            String tikaConfigLoc = (String) initArgs.get(ExtractingRequestHandler.CONFIG_LOCATION);
+            if (tikaConfigLoc != null) {
+              System.setProperty("tika.config", tikaConfigLoc); // see TikaConfig() no-arg constructor impl
+            }
+            
+            NamedList configDateFormats = (NamedList) initArgs.get(ExtractingRequestHandler.DATE_FORMATS);
+            if (configDateFormats != null && configDateFormats.size() > 0) {
+              dateFormats = new HashSet<String>();
+              Iterator<Map.Entry> it = configDateFormats.iterator();
+              while (it.hasNext()) {
+                String format = (String) it.next().getValue();
+                LOGGER.info("Adding Date Format: {}", format);
+                dateFormats.add(format);
+              }
+            }            
+          }
+          break; // found it
+        }
+      }
+    } catch (ParserConfigurationException e) {
+      throw new ConfigurationException(e);
+    } catch (IOException e) {
+      throw new ConfigurationException(e);
+    } catch (SAXException e) {
+      throw new ConfigurationException(e);
+    }
+    
+    try {
+      config = new TikaConfig();
+    } catch (TikaException e) {
+      throw new ConfigurationException(e);
+    } catch (IOException e) {
+      throw new ConfigurationException(e);
+    }
+    autoDetectParser = new AutoDetectParser(config);
+  }
+
+  @Override
+  protected SolrServer createSolrServer() {
+    if (zkConnectString != null) {
+      try {
+        return new CloudSolrServer(zkConnectString);
+      } catch (MalformedURLException e) {
+        throw new ConfigurationException(e);
+      }
+    } else {
+      //return new HttpSolrServer(solrServerUrl);
+      //return new ConcurrentUpdateSolrServer(solrServerUrl, solrServerQueueLength, solrServerNumThreads);
+      return new SafeConcurrentUpdateSolrServer(solrServerUrl, solrServerQueueLength, solrServerNumThreads);
+      //server.setParser(new XMLResponseParser()); // binary parser is used by default
+    }    
+  }  
+
+  @Override
+  protected int getBatchSize() {
+    return solrServerBatchSize;
+  }
+
+  @Override
+  protected List<SolrInputDocument> extract(final Event event) {
+    Parser parser = autoDetectParser;
+    String streamMediaType = event.getHeaders().get(ExtractingParams.STREAM_TYPE);
+    if (streamMediaType != null) {
+      //Cache?  Parsers are lightweight to construct and thread-safe, so I'm told
+      MediaType mt = MediaType.parse(streamMediaType.trim().toLowerCase(Locale.ROOT));
+      parser = new DefaultParser(config.getMediaTypeRegistry()).getParsers().get(mt);
+      if (parser == null) {
+        throw new FlumeException("Stream media type of " + streamMediaType + " didn't match any known parsers. Please supply a better " + ExtractingParams.STREAM_TYPE + " parameter.");
+      }
+    }
+    
+    Metadata metadata = new Metadata();
+
+    // If you specify the resource name (the filename, roughly) with this parameter,
+    // then Tika can make use of it in guessing the appropriate MIME type
+    String resourceName = event.getHeaders().get(Metadata.RESOURCE_NAME_KEY);
+    if (resourceName != null) {
+      metadata.add(Metadata.RESOURCE_NAME_KEY, resourceName);
+    }
+    
+    // Provide stream's content type as hint to Tika for better auto detection
+    String contentType = event.getHeaders().get(Metadata.CONTENT_TYPE); 
+    if (contentType != null) {
+      metadata.add(Metadata.CONTENT_TYPE, contentType);
+    }
+    
+    // Provide stream's charset as hint to Tika for better auto detection
+    String charset = ContentStreamBase.getCharsetFromContentType(contentType);
+    if (charset != null) {
+      metadata.add(Metadata.CONTENT_ENCODING, charset);
+    }
+
+    InputStream inputStream = null;
+    try {
+      inputStream = TikaInputStream.get(event.getBody(), metadata);
+      for (Entry<String, String> entry : event.getHeaders().entrySet()) {
+        metadata.set(entry.getKey(), entry.getValue());
+      }
+      
+      String xpathExpr = params.get(ExtractingParams.XPATH_EXPRESSION);
+//    String xpathExpr = "/xhtml:html/xhtml:body/xhtml:div/descendant:node()"; // params.get(ExtractingParams.XPATH_EXPRESSION);
+//      boolean extractOnly = params.getBool(ExtractingParams.EXTRACT_ONLY, false);
+      
+      SolrContentHandler handler = createSolrContentHandler(metadata);
+      ContentHandler parsingHandler = handler;
+      StringWriter debugWriter = null;
+      if (LOGGER.isDebugEnabled()) {
+        debugWriter = new StringWriter();
+        ContentHandler serializer = new XMLSerializer(debugWriter, new OutputFormat("XML", "UTF-8", true));
+        parsingHandler = new TeeContentHandler(parsingHandler, serializer); 
+      }
+
+      if (xpathExpr != null) {
+        Matcher matcher = PARSER.parse(xpathExpr);
+        parsingHandler = new MatchingContentHandler(parsingHandler, matcher);
+      }
+
+      ParseContext ctx = new ParseContext();//TODO: should we design a way to pass in parse context?
+      ctx.set(Parser.class, parser); // necessary for gzipped files or tar files, etc! copied from TikaCLI
+      ctx.set(TikaSolrSink.class, this);
+      ctx.set(SolrContentHandler.class, handler);
+      ctx.set(AtomicLong.class, new AtomicLong());
+      
+      try {
+        addPasswordHandler(resourceName, ctx);
+        parser.parse(inputStream, parsingHandler, metadata, ctx);
+      } catch (Exception e) {
+        boolean ignoreTikaException = params.getBool(ExtractingParams.IGNORE_TIKA_EXCEPTION, false);
+        if (ignoreTikaException) {
+          LOGGER.warn(new StringBuilder("skip extracting text due to ").append(e.getLocalizedMessage())
+              .append(". metadata=").append(metadata.toString()).toString());
+        } else {
+          throw new FlumeException(e);
+        }
+      }        
+      
+      LOGGER.debug("debug XML doc: {}", debugWriter);        
+      
+      if (ctx.get(MultiDocumentParserMarker.class) != null) {
+        return Collections.EMPTY_LIST;
+      }
+      
+      SolrInputDocument doc = handler.newDocument();
+      LOGGER.debug("solr doc: {}", doc);
+      return Collections.singletonList(doc);
+    } finally {
+      if (inputStream != null) { 
+        try {
+          inputStream.close();
+        } catch (IOException e) {
+          // ignore
+        }
+      }
+    }
+  }
+
+  protected SolrContentHandler createSolrContentHandler(Metadata metadata) {
+    return new SolrContentHandler(metadata, params, schema, dateFormats);
+  }
+
+  protected void addPasswordHandler(String resourceName, ParseContext ctx) throws FileNotFoundException {
+    RegexRulesPasswordProvider epp = new RegexRulesPasswordProvider();
+    String pwMapFile = params.get(ExtractingParams.PASSWORD_MAP_FILE);
+    if (pwMapFile != null && pwMapFile.length() > 0) {
+      InputStream is = new BufferedInputStream(new FileInputStream(pwMapFile)); // getResourceLoader().openResource(pwMapFile);
+      if (is != null) {
+        LOGGER.debug("Password file supplied: {}", pwMapFile);
+        epp.parse(is);
+      }
+    }
+    ctx.set(PasswordProvider.class, epp);
+    String resourcePassword = params.get(ExtractingParams.RESOURCE_PASSWORD);
+    if (resourcePassword != null) {
+      epp.setExplicitPassword(resourcePassword);
+      LOGGER.debug("Literal password supplied for file {}", resourceName);
+    }
+  }
+
+}
