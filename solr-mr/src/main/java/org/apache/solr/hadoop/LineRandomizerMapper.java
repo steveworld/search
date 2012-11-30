0f5828a7d0c4ac3208d98dbddc16a85583edf56b
Wolfgang Hoschek <whoschek@cloudera.com>
2012-11-29 19:55:27 -0800

    * To uniformly spread load across all mappers we randomize solrNlistFile        * with a separate small Mapper & Reducer preprocessing step. This way        * each input line ends up on a random position in the output file list.        * Each mapper indexes a disjoint consecutive set of files such that each        * set has roughtly the same size, at least from a probabilistic        * perspective.

---
+++ solr-mr/src/main/java/org/apache/solr/hadoop/LineRandomizerMapper.java
@@ -0,0 +1,35 @@
+package org.apache.solr.hadoop;
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LineRandomizerMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+
+  private Random random;
+  
+  private static final Logger LOGGER = LoggerFactory.getLogger(LineRandomizerMapper.class);
+
+  @Override
+  protected void setup(Context context) throws IOException, InterruptedException {
+    super.setup(context);
+    long taskId = 0;
+    if (context.getTaskAttemptID() != null) { // MRUnit returns null
+      LOGGER.info("context.getTaskAttemptID().getId(): {}", context.getTaskAttemptID().getId());
+      LOGGER.info("context.getTaskAttemptID().getTaskID().getId(): {}", context.getTaskAttemptID().getTaskID().getId());
+      taskId = context.getTaskAttemptID().getTaskID().getId(); // taskId = 0, 1, ..., N
+    }
+    random = new Random(123456789012345678L + taskId); // deterministic PNRG sequence for easy reproducability
+  }
+
+  @Override
+  protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    LOGGER.info("map key: {}, value: {}", key, value);
+    context.write(new LongWritable(random.nextLong()), value);
+  }
+}
---
+++ solr-mr/src/main/java/org/apache/solr/hadoop/LineRandomizerReducer.java
@@ -0,0 +1,23 @@
+package org.apache.solr.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LineRandomizerReducer extends Reducer<LongWritable, Text, Text, NullWritable> {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(LineRandomizerReducer.class);
+
+  @Override
+  protected void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+    for (Text value : values) {
+      LOGGER.info("reduce key: {}, value: {}", key, value);
+      context.write(value, NullWritable.get());
+    }
+  }
+}
