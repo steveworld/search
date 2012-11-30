31c41158f722f9b9b660d50250e707834fdc8a50
Wolfgang Hoschek <whoschek@cloudera.com>
2012-11-29 20:12:31 -0800

    javadoc

--- solr-mr/src/main/java/org/apache/solr/hadoop/LineRandomizerReducer.java
+++ solr-mr/src/main/java/org/apache/solr/hadoop/LineRandomizerReducer.java
@@ -9,6 +9,14 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * MR Reducer that randomizing a list of URLs.
+ * 
+ * Reducer input is (randomPosition, URL) pairs. Each such pair indicates a file
+ * to index.
+ * 
+ * Reducer output is a list of URLs, each URL in a random position.
+ */
 public class LineRandomizerReducer extends Reducer<LongWritable, Text, Text, NullWritable> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(LineRandomizerReducer.class);
