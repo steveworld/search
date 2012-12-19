27b408d832d01ca2f154cfb4a79c56154cdd1650
Wolfgang Hoschek <whoschek@cloudera.com>
2012-12-18 18:53:14 -0800

    Print some info about the environment in which the unit tests are running

---
+++ solr-mr/src/test/java/org/apache/solr/hadoop/tika/PrintEnvironmentTest.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.solr.hadoop.tika;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.apache.commons.lang.SystemUtils;
+import org.junit.Test;
+
+/** Print some info about the environment in which the unit tests are running */
+public class PrintEnvironmentTest {
+
+  @Test
+  public void testEnvironment() throws UnknownHostException {
+    System.out.println("Running test suite with java version: " + SystemUtils.JAVA_VERSION + " "
+        + SystemUtils.JAVA_VM_NAME + " on " + SystemUtils.OS_NAME + " " + SystemUtils.OS_VERSION + "/"
+        + SystemUtils.OS_ARCH + " on host: " + InetAddress.getLocalHost().getHostName());
+  }
+}
