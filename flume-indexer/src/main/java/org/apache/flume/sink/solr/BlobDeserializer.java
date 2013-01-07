9d2f4f935626f761285f5ac1a60f6b29c200e144
Wolfgang Hoschek <whoschek@cloudera.com>
2013-01-07 14:54:18 -0800

    cleanup

--- flume-indexer/src/main/java/org/apache/flume/sink/solr/BlobDeserializer.java
+++ flume-indexer/src/main/java/org/apache/flume/sink/solr/BlobDeserializer.java
@@ -67,7 +67,6 @@ public class BlobDeserializer implements EventDeserializer {
    * @return Event containing a BLOB
    * @throws IOException
    */
-  @SuppressWarnings("resource")
   @Override
   public Event readEvent() throws IOException {
     ensureOpen();
