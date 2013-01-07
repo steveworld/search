5a5ce473454018a55b05984964c37c97db3d7562
Wolfgang Hoschek <whoschek@cloudera.com>
2013-01-06 20:00:54 -0800

    more logging

--- solr-mr/src/main/java/org/apache/solr/hadoop/tika/TikaMapper.java
+++ solr-mr/src/main/java/org/apache/solr/hadoop/tika/TikaMapper.java
@@ -102,7 +102,7 @@ public class TikaMapper extends SolrMapper<LongWritable, Text> {
       LOG.info("Ignoring file that somehow has been deleted since the job was submitted: {}", path);
       return;
     }
-    LOG.info("Processing {}", path);
+    LOG.info("Processing path {}", path);
     FSDataInputStream in = fs.open(path);
     try {
       Map<String,String> headers = new HashMap<String, String>();
@@ -112,7 +112,7 @@ public class TikaMapper extends SolrMapper<LongWritable, Text> {
       headers.put(Metadata.RESOURCE_NAME_KEY, path.getName()); // Tika can use the file name in guessing the right MIME type
       indexer.process(new StreamEvent(in, headers));
     } catch (SolrServerException e) {
-      LOG.error("Unable to process event ", e);
+      LOG.error("Unable to process path " + path, e);
     } finally {
       in.close();
     }
