197778d2fa6bc3dcf4471c342d55f3c6abcd7c37
Wolfgang Hoschek <whoschek@cloudera.com>
2013-01-16 14:01:45 -0800

    Flume's ResettableInputStream API has recently changed - Implement that new API to prevent test failures

--- flume-indexer/src/test/java/org/apache/flume/sink/solr/ResettableTestStringInputStream.java
+++ flume-indexer/src/test/java/org/apache/flume/sink/solr/ResettableTestStringInputStream.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 
 import org.apache.flume.serialization.ResettableInputStream;
 
-class ResettableTestStringInputStream implements ResettableInputStream {
+class ResettableTestStringInputStream extends ResettableInputStream {
 
   private String str;
   private int markPos = 0;
@@ -49,6 +49,16 @@ class ResettableTestStringInputStream implements ResettableInputStream {
   @Override
   public void reset() throws IOException {
     curPos = markPos;
+  }
+
+  @Override
+  public void seek(long position) throws IOException {
+    throw new UnsupportedOperationException("Unimplemented in test class");
+  }
+
+  @Override
+  public long tell() throws IOException {
+    throw new UnsupportedOperationException("Unimplemented in test class");
   }
 
   @Override
