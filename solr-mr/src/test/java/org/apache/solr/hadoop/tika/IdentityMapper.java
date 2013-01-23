59dcaabf5201f6e8cc1946bf976cb1918e6b5abc
Wolfgang Hoschek <whoschek@cloudera.com>
2013-01-22 23:49:00 -0800

    quarantining unnecessary code

--- solr-mr/src/test/java/org/apache/solr/hadoop/tika/IdentityMapper.java
+++ solr-mr/src/test/java/org/apache/solr/hadoop/tika/IdentityMapper.java
@@ -14,23 +14,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.solr.hadoop;
+package org.apache.solr.hadoop.tika;
 
 import java.io.IOException;
 
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class IdentityReducer extends Reducer<Text, NullWritable, Text, NullWritable> {
+public class IdentityMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(IdentityReducer.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(IdentityMapper.class);
 
   @Override
-  protected void reduce(Text key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
-    LOGGER.info("reduce key: {}, value: {}", key, values);
-    context.write(key, NullWritable.get());
+  protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    LOGGER.info("map key: {}, value: {}", key, value);
+    context.write(value, NullWritable.get());
   }
 }
