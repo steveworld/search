a8f4c4174db24ecbe96cc448e5cd37c259a24778
Wolfgang Hoschek <whoschek@cloudera.com>
2013-03-08 22:17:26 -0800

    remove obsolete import

--- search-mr/src/main/java/org/apache/solr/hadoop/tika/TikaMapper.java
+++ search-mr/src/main/java/org/apache/solr/hadoop/tika/TikaMapper.java
@@ -21,7 +21,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
