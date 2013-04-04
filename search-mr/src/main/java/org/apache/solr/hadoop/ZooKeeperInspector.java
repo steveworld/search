b48387311603a8b1a84209e9396e0053770c7f93
Gregory Chanan <gchanan@cloudera.com>
2013-04-04 14:02:30 -0700

    CDH-11323: MapReduceIndexerTool should be able to download and use solrconfig.xml from zk

--- search-mr/src/main/java/org/apache/solr/hadoop/ZooKeeperInspector.java
+++ search-mr/src/main/java/org/apache/solr/hadoop/ZooKeeperInspector.java
@@ -17,23 +17,33 @@
 
 package org.apache.solr.hadoop;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import org.apache.solr.cloud.ZkController;
 import org.apache.solr.common.SolrException;
 import org.apache.solr.common.cloud.DocCollection;
 import org.apache.solr.common.cloud.Slice;
 import org.apache.solr.common.cloud.SolrZkClient;
 import org.apache.solr.common.cloud.ZkCoreNodeProps;
+import org.apache.solr.common.cloud.ZkNodeProps;
 import org.apache.solr.common.cloud.ZkStateReader;
+import org.apache.zookeeper.KeeperException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.Files;
 
 /**
  * Extracts SolrCloud information from ZooKeeper.
  */
 final class ZooKeeperInspector {
+  private static final Logger LOG = LoggerFactory.getLogger(ZooKeeperInspector.class);
   
   public List<String> extractShardUrls(String zkHost, String collection) {
     DocCollection docCollection = extractDocCollection(zkHost, collection);
@@ -51,18 +61,10 @@ final class ZooKeeperInspector {
   }
   
   public DocCollection extractDocCollection(String zkHost, String collection) {
-    if (zkHost == null) { 
-      throw new IllegalArgumentException("zkHost must not be null");
-    }
-    if (collection == null) { 
+    if (collection == null) {
       throw new IllegalArgumentException("collection must not be null");
     }
-    SolrZkClient zkClient;
-    try {
-      zkClient = new SolrZkClient(zkHost, 15000);
-    } catch (Exception e) {
-      throw new IllegalArgumentException("Cannot connect to ZooKeeper: " + zkHost, e);
-    }
+    SolrZkClient zkClient = getZkClient(zkHost);
     
     try {
       ZkStateReader zkStateReader = new ZkStateReader(zkClient);
@@ -81,7 +83,39 @@ final class ZooKeeperInspector {
       zkClient.close();
     }    
   }
-  
+
+
+  /**
+   * Returns config value given collection name
+   * Borrowed heavily from Solr's ZKController.
+   */
+  public String readConfigName(SolrZkClient zkClient, String collection)
+  throws KeeperException, InterruptedException {
+    if (collection == null) {
+      throw new IllegalArgumentException("collection must not be null");
+    }
+    String configName = null;
+
+    String path = ZkStateReader.COLLECTIONS_ZKNODE + "/" + collection;
+    if (LOG.isInfoEnabled()) {
+      LOG.info("Load collection config from:" + path);
+    }
+    byte[] data = zkClient.getData(path, null, null, true);
+    
+    if(data != null) {
+      ZkNodeProps props = ZkNodeProps.load(data);
+      configName = props.getStr(ZkController.CONFIGNAME_PROP);
+    }
+    
+    if (configName != null && !zkClient.exists(ZkController.CONFIGS_ZKNODE + "/" + configName, true)) {
+      LOG.error("Specified config does not exist in ZooKeeper:" + configName);
+      throw new IllegalArgumentException("Specified config does not exist in ZooKeeper:"
+        + configName);
+    }
+
+    return configName;
+  }
+
   public List<Slice> getSortedSlices(Collection<Slice> slices) {
     List<Slice> sorted = new ArrayList(slices);
     Collections.sort(sorted, new Comparator<Slice>() {
@@ -92,5 +126,43 @@ final class ZooKeeperInspector {
     });
     return sorted;
   }
-  
+
+  /**
+   * Download and return the config directory from ZK
+   */
+  public File downloadConfigDir(SolrZkClient zkClient, String configName)
+  throws IOException, InterruptedException {
+    File dir = Files.createTempDir();
+    try {
+      ZkController.downloadConfigDir(zkClient, configName, dir);
+    } catch (KeeperException ke) {
+      throw new IOException(ke);
+    }
+    File confDir = new File(dir, "conf");
+    if (!confDir.isDirectory()) {
+      // create a temporary directory with "conf" subdir and mv the config in there.  This is
+      // necessary because of CDH-11188; solrctl does not generate nor accept directories with e.g.
+      // conf/solrconfig.xml which is necessary for proper solr operation.  This should work
+      // even if solrctl changes.
+      confDir = new File(Files.createTempDir().getAbsolutePath(), "conf");
+      confDir.getParentFile().deleteOnExit();
+      Files.move(dir, confDir);
+      dir = confDir.getParentFile();
+    }
+    return dir;
+  }
+
+  public SolrZkClient getZkClient(String zkHost) {
+    if (zkHost == null) {
+      throw new IllegalArgumentException("zkHost must not be null");
+    }
+
+    SolrZkClient zkClient;
+    try {
+      zkClient = new SolrZkClient(zkHost, 15000);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Cannot connect to ZooKeeper: " + zkHost, e);
+    }
+    return zkClient;
+  }
 }
