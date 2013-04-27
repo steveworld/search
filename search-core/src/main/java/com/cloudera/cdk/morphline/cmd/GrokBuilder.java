965648b5ab589c7e14fb36d10c41f1e160b41641
Wolfgang Hoschek <whoschek@cloudera.com>
2013-04-26 20:03:33 -0700

    CDH-11747: sort files by name when reading multiple dictionary files - this makes the order in which dictionaries are loaded predictable

--- search-core/src/main/java/com/cloudera/cdk/morphline/cmd/GrokBuilder.java
+++ search-core/src/main/java/com/cloudera/cdk/morphline/cmd/GrokBuilder.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -309,6 +310,7 @@ public final class GrokBuilder implements CommandBuilder {
     private void loadDictionaryFile(File fileOrDir) throws IOException {
       if (fileOrDir.isDirectory()) {
         File[] files = fileOrDir.listFiles();
+        Arrays.sort(files);
         for (File file : files) {
           loadDictionaryFile(file);
         }
