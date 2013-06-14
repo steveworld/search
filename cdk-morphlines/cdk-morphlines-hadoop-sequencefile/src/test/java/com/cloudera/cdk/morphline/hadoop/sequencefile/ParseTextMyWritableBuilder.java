c55f2fff4d971d2dc21252f6c7e8858369c90318
Gregory Chanan <gchanan@cloudera.com>
2013-06-14 10:15:26 -0700

    CDH-12572: Move sequence file parser morphline command into its own package

--- cdk-morphlines/cdk-morphlines-hadoop-sequencefile/src/test/java/com/cloudera/cdk/morphline/hadoop/sequencefile/ParseTextMyWritableBuilder.java
+++ cdk-morphlines/cdk-morphlines-hadoop-sequencefile/src/test/java/com/cloudera/cdk/morphline/hadoop/sequencefile/ParseTextMyWritableBuilder.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.solr.hadoop.morphline.seqfile;
+package com.cloudera.cdk.morphline.hadoop.sequencefile;
 
 import java.io.DataInput;
 import java.io.DataOutput;
