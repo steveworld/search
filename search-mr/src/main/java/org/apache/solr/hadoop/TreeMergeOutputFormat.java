b177187fa8c71134e3a8355d277c6fe0d07e27c0
Wolfgang Hoschek <whoschek@cloudera.com>
2016-03-09 15:48:49 -0800

    CDH-31623: Duration in console log msg is calculated with wrong math

--- search-mr/src/main/java/org/apache/solr/hadoop/TreeMergeOutputFormat.java
+++ search-mr/src/main/java/org/apache/solr/hadoop/TreeMergeOutputFormat.java
@@ -144,7 +144,7 @@ public class TreeMergeOutputFormat extends FileOutputFormat<Text, NullWritable> 
         if (LOG.isDebugEnabled()) {
           context.getCounter(SolrCounters.class.getName(), SolrCounters.LOGICAL_TREE_MERGE_TIME.toString()).increment(System.currentTimeMillis() - start);
         }
-        float secs = (System.nanoTime() - start) / (float)(10^9);
+        float secs = (System.nanoTime() - start) / (float)(1.0e9);
         LOG.info("Logical merge took {} secs", secs);        
         int maxSegments = context.getConfiguration().getInt(TreeMergeMapper.MAX_SEGMENTS_ON_TREE_MERGE, Integer.MAX_VALUE);
         context.setStatus("Optimizing Solr: forcing mtree merge down to " + maxSegments + " segments");
@@ -158,13 +158,13 @@ public class TreeMergeOutputFormat extends FileOutputFormat<Text, NullWritable> 
         if (LOG.isDebugEnabled()) {
           context.getCounter(SolrCounters.class.getName(), SolrCounters.PHYSICAL_TREE_MERGE_TIME.toString()).increment(System.currentTimeMillis() - start);
         }
-        secs = (System.nanoTime() - start) / (float)(10^9);
+        secs = (System.nanoTime() - start) / (float)(1.0e9);
         LOG.info("Optimizing Solr: done forcing tree merge down to {} segments in {} secs", maxSegments, secs);
         
         start = System.nanoTime();
         LOG.info("Optimizing Solr: Closing index writer");
         writer.close();
-        secs = (System.nanoTime() - start) / (float)(10^9);
+        secs = (System.nanoTime() - start) / (float)(1.0e9);
         LOG.info("Optimizing Solr: Done closing index writer in {} secs", secs);
         context.setStatus("Done");
       } finally {
